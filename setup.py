e568ea25ff369731734ebdfd387a5c765de997f4
vv020633 <weebguidance@gmail.com>
2020-10-06 08:52:23 +0100

    Renaming file structure, addition of manifest file and license for building/distributing

--- setup.py
+++ setup.py
@@ -1,21 +1,41 @@
-from distutils.core import setup
+import setuptools
 
-# import build_ui
-try:
-    from pyqt_distutils.build_ui import build_ui
-    cmdclass = {'build_ui': build_ui}
-except ImportError:
-    build_ui = None  # user won't have pyqt_distutils when deploying
-    cmdclass = {}
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
-setup(
-    name='WeebGuidance',
-    version='1.0',
+setuptools.setup(
+    name='Weeb-Guidance',
+    version='1.0.10',
     author='Chas McLaughlin',
     author_email='weebguidance@gmail.com',
-    packages=['weeb-Guidance',],
-    install_requires=['PyQt5', 'python3-bs4'],
-    license='Creative Commons Attribution-Noncommercial-Share Alike license',
-    long_description=open('README.txt').read(),
-    cmdclass=cmdclass,
+    description='A random anime episode selector',
+    long_description=long_description,
+    url="https://github.com/vv020633/myAnimeListV2",
+    license="MIT",
+    packages= setuptools.find_packages(),
+    install_requires=['PyQt5', 'beautifulsoup4','jikanpy'],
+    python_requires='>=3.6',
+    classifiers=[
+        'Environment :: Console',
+        'Programming Language :: Python :: 3',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    entry_points = {
+        'gui_scripts' : [
+            "weeb-guidance = weeb_guidance.__main__:main"
+            ],
+    },
+
+    package_dir={
+        'weeb-guidance': 'weeb-guidance'
+        },
+    package_data = {
+        'weeb-guidance' : ['forms/*.ui', 'icon/*.png' ],
+    },
+    # data_files = {
+    #     ('share/applications/', ['weeb-guidance.desktop'])
+    # }
+    # cmdclass=cmdclass,
 )
